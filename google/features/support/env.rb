82b871e588cf21d8a246a31083f6df696d92a2d6
Ben Hall <Ben@Benhall.me.uk>
2009-12-29 12:51:36 +0000

    First Google Example

---
+++ google/features/support/env.rb
@@ -0,0 +1,17 @@
+require "spec/expectations"
+require "webrat"
+ 
+Webrat.configure do |config|
+  config.mode = :mechanize
+end
+
+class MechanizeWorld < Webrat::MechanizeAdapter
+  include Webrat::Matchers
+  include Webrat::Methods
+  # no idea why we need this but without it response_code is not always recognized
+  Webrat::Methods.delegate_to_session :response_code, :response_body
+end
+
+World do
+  MechanizeWorld.new
+end
